f87203cbc7652ba1773e20c4162cb30dc5397a96
DiegoLG15 <a20200839@pucp.edu.pe>
2023-06-07 17:58:22 -0500

    first commit

---
+++ labHiro_20200839/src/test/java/com/example/labhiro_20200839/LabHiro20200839ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labhiro_20200839;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabHiro20200839ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
